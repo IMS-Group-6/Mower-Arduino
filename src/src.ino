209b407b0ce2dd9571d08cf59da051b725be4834
baan20ty <baan20ty@student.ju.se>
2023-04-03 14:21:13 +0200

    Changed to encoder motor initiation

--- src/src.ino
+++ src/src.ino
@@ -1,46 +1,48 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
-#include <MeMCore.h>
+#include <MeAuriga.h>
 
-MeDCMotor motorLeft(M1);
-MeDCMotor motorRight(M2);
+MeEncoderOnBoard Encoder_1(SLOT1);
+MeEncoderOnBoard Encoder_2(SLOT2);
 MeUltrasonicSensor ultrasonicSensor(PORT_3);
 
 SoftwareSerial RaspberryPiSerial(10, 11); // RX, TX
 
 void setup() {
   Serial.begin(9600);
-  RaspberryPiSerial.begin(9600);
+  // RaspberryPiSerial.begin(9600);
 }
 
-void controlMotors(int leftSpeed, int rightSpeed) {
-  motorLeft.run(leftSpeed);
-  motorRight.run(rightSpeed);
+void controlMotors(int left, int right) {
+  Encoder_1.setMotorPwm(left);
+  Encoder_2.setMotorPwm(right);
 }
 
 void sendUltrasonicData() {
   int distance = ultrasonicSensor.distanceCm();
-  RaspberryPiSerial.print("U:");
-  RaspberryPiSerial.println(distance);
+  Serial.print("U:");
+  Serial.println(distance);
 }
 
 void loop() {
-  if (RaspberryPiSerial.available()) {
-    String command = RaspberryPiSerial.readStringUntil('\n');
+  if(Serial.available()){
+    String cmd = Serial.readStringUntil('\n');
 
-    if (command == "forward") {
-      controlMotors(100, 100);
-    } else if (command == "backward") {
-      controlMotors(-100, -100);
-    } else if (command == "left") {
-      controlMotors(-100, 100);
-    } else if (command == "right") {
-      controlMotors(100, -100);
-    } else if (command == "stop") {
-      controlMotors(0, 0);
+    if(cmd=="w"){
+      controlMotors(-180, 180);
+    }
+    else if(cmd=="w"){
+      controlMotors(180, -180);
+    }
+    else if(cmd=="a"){
+      controlMotors(-180, 0);
+    }
+    else if(cmd=="d"){
+      controlMotors(0, 180);
+    }
+    else if(cmd=="stop"){
+      controlMotors(0,0);
     }
   }
-
-  sendUltrasonicData();
-  delay(100);
+  delay(200);
 }
