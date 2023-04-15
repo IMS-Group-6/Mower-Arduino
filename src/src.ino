4ae77dc6b0521f5de7be40d35f9f166adfaa26e5
baan20ty <baan20ty@student.ju.se>
2023-04-15 15:01:08 +0200

    Refactor code and tested code for bugs

--- src/src.ino
+++ src/src.ino
@@ -4,212 +4,25 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
 
-//#define DEBUG_INFO
-//#define DEBUG_INFO1
 
+//  Sensor initiation
+MeUltrasonicSensor ultraSonic(PORT_10);
+MeLineFollower greyScale(PORT_9);
+MeGyro gyroMeter(1, 0x69);
 
-MeUltrasonicSensor *us = NULL;  //PORT_10
-Me7SegmentDisplay seg;
-MePort generalDevice;
-MeLEDMatrix ledMx;
-MeInfraredReceiver *ir = NULL;  //PORT_8
-MeGyro gyro_ext(0,0x68);  //external gryo sensor
-MeGyro gyro(1,0x69);      //On Board external gryo sensor // Uses
-MeCompass Compass;
-MeJoystick joystick;
-MeStepper steppers[4];
-MeBuzzer buzzer;
-MeHumiture humiture;
-MeFlameSensor FlameSensor;
-MeGasSensor GasSensor;
-MeTouchSensor touchSensor;
-Me4Button buttonSensor;
-MeEncoderOnBoard Encoder_1(SLOT1); // Uses
-MeEncoderOnBoard Encoder_2(SLOT2); // Uses
-MeLineFollower line(PORT_9);
-MeEncoderMotor encoders[2]; // Uses
-MePm25Sensor *pm25sensor = NULL;
-MeSmartServo *mysmartservo = NULL;
+//  Motor
+MeEncoderOnBoard Encoder_1(SLOT1);
+MeEncoderOnBoard Encoder_2(SLOT2);
+MeEncoderMotor encoders[2];
 
-typedef struct MeModule
-{
-  int16_t device;
-  int16_t port;
-  int16_t slot;
-  int16_t pin;
-  int16_t index;
-  float values[3];
-} MeModule;
+//  Variables - Integers
+int16_t moveSpeed   = 160;
+int16_t turnSpeed   = 160;
+int16_t minSpeed    = 45;
 
-union
-{
-  uint8_t byteVal[4];
-  float floatVal;
-  long longVal;
-}val;
+// Variables - Double
 
-union
-{
-  uint8_t byteVal[8];
-  double doubleVal;
-}valDouble;
-
-union
-{
-  uint8_t byteVal[2];
-  int16_t shortVal;
-}valShort;
-MeModule modules[12];
-#if defined(__AVR_ATmega32U4__) 
-  int16_t analogs[12]={A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11};
-#endif
-#if defined(__AVR_ATmega328P__) or defined(__AVR_ATmega168__)
-  int16_t analogs[8]={A0,A1,A2,A3,A4,A5,A6,A7};
-#endif
-#if defined(__AVR_ATmega1280__)|| defined(__AVR_ATmega2560__)
-  int16_t analogs[16]={A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15};
-#endif
-
-int16_t len = 52;
-int16_t servo_pins[12]={0,0,0,0,0,0,0,0,0,0,0,0};
-//Just for Auriga
-int16_t moveSpeed = 180;
-int16_t turnSpeed = 180;
-int16_t minSpeed = 45;
-int16_t factor = 23;
-int16_t distance=0;
-int16_t randnum = 0;                                                                               
-int16_t auriga_power = 0;
-int16_t LineFollowFlag=0;
-#define MOVE_STOP       0x00
-#define MOVE_FORWARD    0x01
-#define MOVE_BACKWARD   0x02
-
-#define BLUETOOTH_MODE                       0x00
-#define AUTOMATIC_OBSTACLE_AVOIDANCE_MODE    0x01
-#define BALANCED_MODE                        0x02
-#define IR_REMOTE_MODE                       0x03
-#define LINE_FOLLOW_MODE                     0x04
-#define MAX_MODE                             0x05
-
-#define POWER_PORT                           A4
-#define BUZZER_PORT                          45
-#define RGBLED_PORT                          44 //Uses
-#define RINGALLEDS                           0
-#define AURIGARINGLEDNUM                     12
-
-uint8_t command_index = 0;
-uint8_t auriga_mode = BLUETOOTH_MODE;
-uint8_t index = 0;
-uint8_t dataLen;
-uint8_t modulesLen=0;
-uint8_t irRead = 0;
-uint8_t prevc=0;
-uint8_t keyPressed = KEY_NULL;
-uint8_t serialRead;
-uint8_t buffer[52];
-
-double  lastTime = 0.0;
-double  currentTime = 0.0;
-double  CompAngleY, CompAngleX, GyroXangle;
-double  LastCompAngleY, LastCompAngleX, LastGyroXangle;
-double  last_turn_setpoint_filter = 0.0;
-double  last_speed_setpoint_filter = 0.0;
-double  last_speed_error_filter = 0.0;
-double  speed_Integral_average = 0.0;
-double  angle_speed = 0.0;
-double  balance_car_speed_offsets = 0.0;
-
-float angleServo = 90.0;
-float dt;
-
-long lasttime_angle = 0;
-long lasttime_speed = 0;
-long update_sensor = 0;
-long blink_time = 0;
-long rxruntime = 0;
-long lasttime_receive_cmd = 0;
-long last_Pulse_pos_encoder1 = 0;
-long last_Pulse_pos_encoder2 = 0;
-
-boolean isStart = false;
-boolean isAvailable = false;
-boolean leftflag;
-boolean rightflag;
-boolean start_flag = false;
-boolean move_flag = false;
-boolean boot_show_flag = true;
-boolean blink_flag = false;
-
-String mVersion = "09.01.016";
-
-
-//////////////////////////////////////////////////////////////////////////////////////
-float RELAX_ANGLE = -1;                    //Natural balance angle,should be adjustment according to your own car
-#define PWM_MIN_OFFSET_BIAS   5
-
-#define VERSION                0
-#define ULTRASONIC_SENSOR      1
-#define TEMPERATURE_SENSOR     2
-#define LIGHT_SENSOR           3
-#define POTENTIONMETER         4
-#define JOYSTICK               5
-#define GYRO                   6
-#define SOUND_SENSOR           7
-#define RGBLED                 8
-#define SEVSEG                 9
-#define MOTOR                  10
-#define SERVO                  11
-#define ENCODER                12
-#define IR                     13
-#define IRREMOTE               14
-#define PIRMOTION              15
-#define INFRARED               16
-#define LINEFOLLOWER           17
-#define IRREMOTECODE           18
-#define SHUTTER                20
-#define LIMITSWITCH            21
-#define BUTTON                 22
-#define HUMITURE               23
-#define FLAMESENSOR            24
-#define GASSENSOR              25
-#define COMPASS                26
-#define TEMPERATURE_SENSOR_1   27
-#define DIGITAL                30
-#define ANALOG                 31
-#define PWM                    32
-#define SERVO_PIN              33
-#define TONE                   34
-#define BUTTON_INNER           35
-#define ULTRASONIC_ARDUINO     36
-#define PULSEIN                37
-#define STEPPER                40
-#define LEDMATRIX              41
-#define TIMER                  50
-#define TOUCH_SENSOR           51
-#define JOYSTICK_MOVE          52
-#define COMMON_COMMONCMD       60
-  //Secondary command
-  #define SET_STARTER_MODE     0x10
-  #define SET_AURIGA_MODE      0x11
-  #define SET_MEGAPI_MODE      0x12
-  #define GET_BATTERY_POWER    0x70
-  #define GET_AURIGA_MODE      0x71
-  #define GET_MEGAPI_MODE      0x72
-#define ENCODER_BOARD          61
-  //Read type
-  #define ENCODER_BOARD_POS    0x01
-  #define ENCODER_BOARD_SPEED  0x02
-
-#define ENCODER_PID_MOTION     62
-  //Secondary command
-  #define ENCODER_BOARD_POS_MOTION         0x01
-  #define ENCODER_BOARD_SPEED_MOTION       0x02
-  #define ENCODER_BOARD_PWM_MOTION         0x03
-  #define ENCODER_BOARD_SET_CUR_POS_ZERO   0x04
-  #define ENCODER_BOARD_CAR_POS_MOTION     0x05
-
-MeLineFollower lineFollower (PORT_9);
+// RGB Matrix
 MeRGBLed led_ring(0,12);
 
 
@@ -219,21 +32,16 @@ void updateSpeed(void);
 
 void setup() {
   Serial.begin(115200);
-  led_ring.setpin(RGBLED_PORT);
-  gyro.begin();
+  gyroMeter.begin();
   
 
-  encoders[0] = MeEncoderMotor(SLOT_1);
-  encoders[1] = MeEncoderMotor(SLOT_2);
-  encoders[0].begin();
-  encoders[1].begin();
-  
-  
+  encoders[0], encoders[1] = MeEncoderMotor(SLOT1), MeEncoderMotor(SLOT2);
+  encoders->begin();
+
   wdt_reset();
-  encoders[0].runSpeed(0);
-  encoders[1].runSpeed(0);
+  encoders->runSpeed(0);
 
-  //Set Pwm 8KHz
+  /* //Set Pwm 8KHz
   TCCR1A = _BV(WGM10);
   TCCR1B = _BV(CS11) | _BV(WGM12);
 
@@ -241,31 +49,11 @@ void setup() {
   TCCR2B = _BV(CS21);
   while (!Serial) {
     ; // wait for serial port to connect via USB
-  }
+  } */
 }
 
-
-int16_t dist(){
-  MeUltrasonicSensor u_sensor(PORT_10);
-  int ret = u_sensor.distanceCm();
-  return ret;
-}
-
-double angleX(){
-  gyro.update();
-  return gyro.getAngleX();
-}
-
-
-double angleY(){
-  gyro.update();
-  return gyro.getAngleX();
-}
-
-
-double angleZ(){
-  gyro.update();
-  return gyro.getAngleX();
+void update(void){
+  gyroMeter.update();
 }
 
 void Forward(void)
@@ -295,23 +83,55 @@ void StopMotor(void){
   Encoder_2.setMotorPwm(0);
 }
 
+double getGyroX(){
+  gyroMeter.update();
+  return gyroMeter.getGyroX();
+}
+double getAngleX(){
+  gyroMeter.update();
+  return gyroMeter.getAngleX();
+}
+double getGyroY() {
+  gyroMeter.update();
+  return gyroMeter.getGyroY();
+}
+
+double getAngleY() {
+  gyroMeter.update();
+  return gyroMeter.getAngleY();
+}
+
+double getAngleZ() {
+  gyroMeter.update();
+  return gyroMeter.getAngleZ();
+}
+
+int16_t dist(){
+    return ultraSonic.distanceCm();
+}
+int16_t lineFlag(){
+    return greyScale.readSensors();
+}
+
+
 void loop() {
 
   char cmd;
+
 
   if(dist()<=15){
     StopMotor();
     return;
   } 
 
-  if(lineFollower.readSensors()<=0){
+  if(lineFlag()<=0){
     StopMotor();
     return;
   }
 
   if (Serial.available() >0){
       cmd = Serial.read();
-      Serial.print("Serial available");
+      Serial.println("Serial available");
     
     switch(cmd){
       case 'w':
@@ -327,9 +147,11 @@ void loop() {
         TurnRight();
         break;
       default:
-        Serial.write("No commands", 12);
+        Serial.write("Unknown command ");
+        Serial.println();
+        StopMotor();
         break;
     }  
   }
-  delay(100);
+  delay(50);
 }
